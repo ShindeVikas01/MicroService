8671ba868d5762365faaeb82e8ec3ba6882bc257
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-08-23 01:00:09 +0530

    [Added]:[Vikas] Create Custom Query for get Orders by customer Id

--- CustomerService/src/main/java/com/example/customerservice/customercontroller/OrderController.java
+++ CustomerService/src/main/java/com/example/customerservice/customercontroller/OrderController.java
@@ -5,6 +5,8 @@ import com.example.customerservice.repo.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/order/api")
 public class OrderController {
@@ -14,5 +16,15 @@ public class OrderController {
     @PostMapping("/addorder")
     public OrderEntity addorder(@RequestBody OrderEntity orderEntity){
         return orderRepo.save(orderEntity);
+    }
+
+    @GetMapping("/getorderbyoid/{oid}")
+    public OrderEntity getorderbyoid(@PathVariable("oid")int oid){
+        return orderRepo.findById(oid).get();
+    }
+
+    @GetMapping("/getorderbycid/{custid}")
+    public List<OrderEntity> getorderbycid(@PathVariable("custid")int custid){
+        return orderRepo.findOrderByCid(custid);
     }
 }
--- CustomerService/src/main/java/com/example/customerservice/repo/OrderRepo.java
+++ CustomerService/src/main/java/com/example/customerservice/repo/OrderRepo.java
@@ -2,8 +2,14 @@ package com.example.customerservice.repo;
 
 import com.example.customerservice.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
+
 public interface OrderRepo extends JpaRepository<OrderEntity,Integer> {
+    @Query(nativeQuery = true,value = "SELECT * from orderstb where CID=:custid")
+    List<OrderEntity> findOrderByCid(@Param("custid") int custid);
 }
