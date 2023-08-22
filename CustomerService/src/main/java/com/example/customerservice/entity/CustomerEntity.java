96c2a2eb937183d8f52ad1dfc83d7e9e05addff0
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-08-22 23:11:20 +0530

    [Added]:[Vikas] Customer and Order Enitity Mapping

--- CustomerService/src/main/java/com/example/customerservice/entity/CustomerEntity.java
+++ CustomerService/src/main/java/com/example/customerservice/entity/CustomerEntity.java
@@ -5,9 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name="customertb")
 public class CustomerEntity {
@@ -17,4 +18,19 @@ public class CustomerEntity {
     private int cid;
     private String cname;
     private String cmobno;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderEntity> orderList;
+
+    public CustomerEntity(int cid, String cname, String cmobno) {
+        this.cid = cid;
+        this.cname = cname;
+        this.cmobno = cmobno;
+    }
+
+    public CustomerEntity(int cid, String cname, String cmobno, List<OrderEntity> orderList) {
+        this.cid = cid;
+        this.cname = cname;
+        this.cmobno = cmobno;
+        this.orderList = orderList;
+    }
 }
---
+++ CustomerService/src/main/java/com/example/customerservice/entity/OrderEntity.java
@@ -0,0 +1,25 @@
+package com.example.customerservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="orderstb")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int oid;
+    private String odate;
+    private String oitem;
+    private int oamount;
+    @ManyToOne
+    @JoinColumn(name = "cid")
+    private CustomerEntity customerEntity;
+
+
+}
