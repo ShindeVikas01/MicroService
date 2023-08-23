9305a90c88d3decb40ccbf72c4d2a0040cc3b155
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-08-23 23:06:40 +0530

    [Added]:[Vikas] Eureka Server Implementation and add fetech both orderservice and orderservice

--- CustomerService/src/main/java/com/infinity/customerservice/customercontroller/CustomerController.java
+++ CustomerService/src/main/java/com/infinity/customerservice/customercontroller/CustomerController.java
@@ -31,7 +31,7 @@ public class CustomerController {
     @GetMapping("/getcustomerbyid/{cid}")
     public CustomerEntity getcustomerbyid(@PathVariable("cid")int cid){
         CustomerEntity customer=customerRepo.findById(cid).get();
-        List<OrderEntity> order=restTemplate.getForObject("http://localhost:8001/order/api/getorderbycid/"+cid,List.class);
+        List<OrderEntity> order=restTemplate.getForObject("http://orderservice/order/api/getorderbycid/"+cid,List.class);
         customer.setOrderList(order);
         return customer;
     }
